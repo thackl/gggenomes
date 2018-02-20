f6927e09836dd5a8fcb3bb424671cb8e3f4635b6
Thomas Hackl <thomas.hackl@uni-wuerzburg.de>
2018-02-20 10:36:43 -0500

    simple interface to list example data sets and files located in extdata

---
+++ R/examples.R
@@ -0,0 +1,23 @@
+#' Example data sets
+#'
+#' list available example data sets
+#' @export
+#' @importFrom tibble as_tibble
+example_sets <- function(){
+    write('# TODO: df with desc', stderr())
+    example_dir <- system.file(paste0("extdata/"), package="gggenomes")
+    cat('# Reading example data from:\n# ', example_dir, '\n', sep="")
+    example_sets <- list.files(example_dir, recursive=FALSE)
+    #cat(paste0(example_sets, '\n', collapse=""))
+    as_tibble(example_sets)
+}
+
+#' list files from example data sets
+#'
+#' @param set data set, see `example_sets` for a list
+#' @param patt wildcard pattern to filter files
+#' @export
+example_files <- function(set, patt = "*"){
+    example_dir <- system.file(paste0("extdata/", set), package="gggenomes")
+    list.files(example_dir, patt, recursive=FALSE, full.names=TRUE)
+}
