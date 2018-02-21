75036ed31c95b34cc84d4b7b5b7c4e90dc56e53d
Thomas Hackl <thomas.hackl@uni-wuerzburg.de>
2018-02-21 07:20:04 -0500

    geom_feature doc

--- R/gggenomes.R
+++ R/gggenomes.R
@@ -13,7 +13,7 @@ gggenomes <- function(data = NULL, mapping = aes(),
     theme = c("clean", NULL), theme_args = list(),
     ..., environment = parent.frame()){
 
-    if(!any(class(data) == "tbl_genomes")) data <- as_tbl_genomes(data)
+    if(!class(data) == "tbl_genomes") data <- as_tbl_genomes(data)
 
     p <- ggplot(data = data, mapping = mapping, environment = environment)
 
