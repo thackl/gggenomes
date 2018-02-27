87309993eee363d4dd8b8644665372ee51c5b137
Thomas Hackl <thomas.hackl@uni-wuerzburg.de>
2018-02-26 19:18:16 -0500

    geoms w/o stat offset

--- R/geom.R
+++ R/geom.R
@@ -1,53 +1,38 @@
-#' draw contigs
+#' draw chromosomes
 #'
 #' @param data contig_layout
 #' @param arrow set to non-NULL to generate default arrows
 #' @inheritParams ggplot2::geom_segment
 #' @importFrom ggplot2 geom_segment
 #' @export
-geom_contig <- function(mapping = NULL, data = expose_data(contigs),
-    stat = StatOffsetLength, arrow = NULL, ...){
-
-    default_aes <- aes_(y=~gix, yend=~gix, offset=~goffset+coffset,
-                        length=~clength, strand=~gstrand*cstrand)
-    mapping <- aesIntersect(mapping, default_aes)
-
-    # default arrow
-    if (!is_null(arrow) & !inherits(arrow, "arrow"))
-        arrow <- arrow(length = unit(3, "pt"))
-
-    geom_segment(mapping = mapping, data = data, stat = stat, arrow = arrow, ...)
-}
-
-#' @export
 geom_chromosomes <- function(mapping = NULL, data = expose_data(chromosomes),
     arrow = NULL, ...){
 
-    default_aes <- aes_(x=~x,xend=~xend,y=~y,yend=~y)
-    mapping <- aesIntersect(mapping, default_aes)
+  default_aes <- aes_(x=~x,xend=~xend,y=~y,yend=~y)
+  mapping <- aesIntersect(mapping, default_aes)
 
-    # default arrow
-    if (!is_null(arrow) & !inherits(arrow, "arrow"))
+  # default arrow
+  if (!is_null(arrow) & !inherits(arrow, "arrow"))
         arrow <- arrow(length = unit(3, "pt"))
 
-    geom_segment(mapping = mapping, data = data, arrow = arrow, ...)
+  geom_segment(mapping = mapping, data = data, arrow = arrow, ...)
 }
 
 #' draw features
 #'
 #' @param data feature_layout
+#' @export
 geom_feature <- function(mapping = NULL, data = expose_data(features),
-    stat=StatOffsetRange, arrow = NULL, ...){
+    arrow = NULL, ...){
 
-    default_aes <- aes_(y=~gix, yend=~gix, offset=~foffset,
-                        start=~fstart, end=~fend, strand=~fstrand)
-    mapping <- gggenomes:::aesIntersect(mapping, default_aes)
+  default_aes <- aes_(x=~x, xend=~xend, y=~y, yend=~y, size=3)
+  mapping <- gggenomes:::aesIntersect(mapping, default_aes)
 
-    # default arrow
-    if (!is_null(arrow) & !inherits(arrow, "arrow"))
-        arrow <- arrow(length = unit(3, "pt"))
+  # default arrow
+  if (!is_null(arrow) & !inherits(arrow, "arrow"))
+    arrow <- arrow(length = unit(3, "pt"))
 
-    geom_segment(mapping = mapping, data = data, stat = stat, arrow = arrow, ...)
+  geom_segment(mapping = mapping, data = data, arrow = arrow, ...)
 }
 
 #' draw genes
@@ -56,13 +41,12 @@ geom_feature <- function(mapping = NULL, data = expose_data(features),
 #' @inheritParams gggenes::geom_gene_arrow
 #' @importFrom gggenes geom_gene_arrow
 #' @export
-geom_gene <- function(mapping = NULL, data = expose_data(features),
-    stat = StatOffsetGene, ...){
+geom_gene <- function(mapping = NULL, data = expose_data(features), ...){
 
-    default_aes <- aes_(y=~gix,start=~fstart,end=~fend,offset=~foffset,strand=~fstrand)
-    mapping <- aesIntersect(mapping, default_aes)
+  default_aes <- aes_(y=~y,xmin=~x,xmax=~xend)
+  mapping <- aesIntersect(mapping, default_aes)
 
-    gggenes::geom_gene_arrow(mapping = mapping, data = data, stat = stat, ...)
+  gggenes::geom_gene_arrow(mapping = mapping, data = data, ...)
 }
 
 #' draw links
@@ -73,21 +57,22 @@ geom_gene <- function(mapping = NULL, data = expose_data(features),
 #' @importFrom ggplot2 geom_polygon
 #' @export
 geom_link <- function(mapping = NULL, data = expose_data(links), nudge_frac=.1, ...){
-    write('# TODO: StatOffset', stderr())
-    mapping <- aesIntersect(mapping, aes_(x=~x, y=quo(gix + nudge_sign * !!nudge_frac), group=~lix))
 
-    geom_polygon(mapping = mapping, data=data, ...)
+  default_aes <- aes_(x=~x, y=quo(gix + nudge_sign * !!nudge_frac), group=~lix)
+  mapping <- aesIntersect(mapping, default_aes)
+
+  geom_polygon(mapping = mapping, data=data, ...)
 }
 
 #' draw feature labels
 #'
 #' @export
 geom_feature_label <- function(mapping = NULL, data = expose_data(features),
-    stat = StatOffsetRange, angle = 45,hjust = 0, nudge_y = 0.2, size = 2, ...){
+    angle = 45,hjust = 0, nudge_y = 0.2, size = 2, ...){
 
-    default_aes <- aes_(y=~gix,start=~(fstart+fend)/2,offset=~foffset,strand=1)
-    mapping <- aesIntersect(mapping, default_aes)
+  default_aes <- aes_(y=~y,x=~(x+xend)/2)
+  mapping <- aesIntersect(mapping, default_aes)
 
-    geom_text(mapping = mapping, data = data, stat = stat,
-        angle = angle, hjust = hjust, nudge_y = nudge_y, size = size, ...)
+  geom_text(mapping = mapping, data = data, angle = angle, hjust = hjust,
+            nudge_y = nudge_y, size = size, ...)
 }
