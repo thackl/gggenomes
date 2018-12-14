c695d8cfd750e88c321d0db4bd5497c451d4d5d5
Thomas Hackl <thackl@lim4.de>
2018-12-14 08:20:16 -0500

    better links - with and without coordinates

--- R/tbl_link.R
+++ R/tbl_link.R
@@ -16,8 +16,7 @@ as_links.default <- function(x, contigs, ..., everything=TRUE){
 as_links.tbl_df <- function(x, contigs, ..., everything=TRUE){
   if(!inherits(contigs, "tbl_contig")) stop("expecting contigs as tbl_contig")
 
-  vars <- c("query_contig_id", "query_start", "query_end",
-            "target_contig_id", "target_start","target_end")
+  vars <- c("query_contig_id", "target_contig_id")
   require_genome_id <- attr(contigs, "require_genome_id")
   if(!is.null(require_genome_id) && require_genome_id)
     vars <- c(vars, "query_genome_id", "target_genome_id")
@@ -38,21 +37,40 @@ as_links.tbl_df <- function(x, contigs, ..., everything=TRUE){
 
 # recompute layout
 #' @export
-layout.tbl_link <- function(x, ...){
+as_links.tbl_link <- function(x, ...){
     stop("TODO")
+}
+
+as_feature_links <- function(x, features, ..., everything=TRUE){
+  if(!inherits(contigs, "tbl_contig")) stop("expecting contigs as tbl_contig")
+  if(!inherits(features, "tbl_feature")) stop("expecting featuress as tbl_feature")
 }
 
 #' Layout tbl_link
 #'
 #' @inheritParams as_links
 #' @param ... not used
-layout.tbl_link <- function(x, contigs, ...){
-  .x <- x
-  x %<>%
-    inner_join(contigs %>% ungroup %>% select(query_contig_id=contig_id, q.gix=.gix)) %>%
-    inner_join(contigs %>% ungroup %>% select(target_contig_id=contig_id, t.gix=.gix)) %>%
-    filter(abs(t.gix-q.gix)==1) # filter links between non-adjacent contig_layout
-  
+layout.tbl_link <- function(x, contigs, features=NULL, adjacent_only=TRUE, ...){
+  x_orig <- x
+  contigs %<>% ungroup
+
+  if(!has_vars(x, c("query_start", "query_end", "target_start", "target_end"))){
+    if(has_vars(x, c("query_start", "query_end", "target_start", "target_end"),
+                any=TRUE)){ stop("Need either all of query_start,query_end,target_start,target_end or none!")}
+    query_contigs <- select(contigs, query_contig_id=contig_id, q.gix=.gix,
+           query_end=length) %>% mutate(query_start=0)
+    target_contigs <- select(contigs, target_contig_id=contig_id, t.gix=.gix,
+        target_end=length) %>% mutate(target_start=0)
+  }else{
+    query_contigs <- select(contigs, query_contig_id=contig_id, q.gix=.gix)
+    target_contigs <- select(contigs, target_contig_id=contig_id, t.gix=.gix)
+  }
+  x %<>% inner_join(query_contigs) %>% inner_join(target_contigs)
+
+  # adjacent links
+  if(adjacent_only) # remove links between non-adjacent contig_layout
+    x %<>% filter(abs(t.gix-q.gix)==1)
+
   if(nrow(x)==0){
     stop("No links found between adjacent genomes in provided contig_layout, consider reordering genomes")
   }
@@ -63,24 +81,20 @@ layout.tbl_link <- function(x, contigs, ...){
   }else{
     x$.strand <- as_numeric_strand(x$strand)
   }
-  
+
   x %<>% mutate(.lix=row_number()) %>%
-    gather(".pid", "x", query_start, query_end, target_start, target_end) %>%
+    # polygon-id - order of points in polygon
+    gather(".pid", "x", query_start, query_end, target_end, target_start) %>%
     mutate(contig_id=ifelse(.pid %in% c("query_start", "query_end"), query_contig_id, target_contig_id)) %>%
     select(-starts_with("q."), -starts_with("t.")) %>%
     arrange(.lix) %>%
-    inner_join(contigs %>% ungroup %>% select(genome_id, contig_id, .gix, .offset)) %>%
+    inner_join(select(contigs, genome_id, contig_id, .gix, .offset)) %>%
     mutate(x=x+.offset) %>%
-    arrange(.lix, .gix) 
+    arrange(.lix, .gix)
 
     x$.pix <- rep(1:4, nrow(x)/4)
     x$.pix[x$.pix==3 & x$.strand < 0] <- 5
     x$.nudge_sign <- rep(c(1,1,-1,-1), nrow(x)/4)
 
-  list(x, .x) # return tbl_link and orig links
-}
-
-
-as_feature_links <- function(){
-
+  list(x, x_orig) # return tbl_link and orig links
 }
