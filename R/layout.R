9ed9421f2e1b3a9ce894ddbf68074da6ff228ecc
Thomas Hackl <thackl@lim4.de>
2019-01-18 13:48:10 -0500

    set center for focus()

--- R/layout.R
+++ R/layout.R
@@ -59,7 +59,6 @@ flip <- function(x, genomes=NULL, ...){
   l <- x$data$contigs %>%
     drop_layout(keep="strand") %>%
     thacklr::split_by(genome_id)
-  print(names(l))
   # flip entire genomes
   if(!is.null(genomes)){
     for(gid in str_subset_unique(names(l), genomes)){
@@ -88,32 +87,48 @@ flip <- function(x, genomes=NULL, ...){
   layout(x)
 }
 
+
+center <- function(min, max, strand, length, center=c("center", "left", "right")){
+  center <- match.arg(center)
+  if(center == "center") ifelse(strand < 0, round(length-mean(c(min, max))), round(mean(c(min, max))))
+  else if(center == "left") ifelse(strand < 0, length-max, min)
+  else if(center == "right") ifelse(strand < 0, length-min, max)
+  else stop(glue("unknown center parameter {center}"))
+}
+
 #' @export
-focus <- function(x, ..., track_id="genes", plus=2000){
+focus <- function(x, ..., track_id="genes", plus=2000, center=c("center", "left", "right")){
   if(length(plus==1)) plus <- c(plus,plus)
 
   # Specifu 'ID' column - so far not required for features
   bounds <- filter(x$data[[track_id]], ...) %>%
     group_by(genome_id, contig_id) %>%
     summarize(
-      min=min(start) - plus[1],
-      max=max(end) + plus[2]) %>%
-    left_join(select(x$data$contigs, genome_id, contig_id, length), by=c("genome_id", "contig_id")) %>%
+      min=min(start), min_plus=min - plus[1],
+      max=max(end), max_plus=max + plus[2]) %>%
+     left_join(select(x$data$contigs, genome_id, contig_id, length, .strand=strand), by=c("genome_id", "contig_id")) %>%
     mutate(
-      min = if_else(min<0,0,min),
-      max = if_else(length < max, length, max))
+      center = center(min, max, .strand, length, center),
+      min_plus = if_else(min_plus<0,0,min_plus),
+      max_plus = if_else(length < max_plus, length, max_plus))
 
   # make sure plus is >0, <contig_length
-  x$data$genes %<>% left_join(bounds, by=c("genome_id", "contig_id")) %>%
-    print %T>%
-    filter(start > min & start < max & end > min & end < max) %>%
-    mutate(start = start-min, end = end-min, min=NULL, max=NULL) %>%
-    add_class("tbl_feature")
+  for (track_id in names(x$data)[-1]){ # first is contigs
+    if(inherits(x$data[[track_id]], "tbl_feature")){
+      #print(paste(track_id, "- recomputing layout"))
+      x$data[[track_id]] %<>% left_join(bounds, by=c("genome_id", "contig_id")) %>%
+        filter(start >= min_plus & start <= max_plus & end >= min_plus & end <= max_plus) %>%
+        #mutate(start = start-center, end = end-center, min=NULL, max=NULL) %>%
+        add_class("tbl_feature")
+    }
+  }
 
   x$data$contigs %<>%
-    inner_join(bounds) %>%
-      mutate(length=max-min+1, min=NULL, max=NULL) %>%
-      add_class("tbl_contig") %>% layout
+    inner_join(select(bounds, center)) %>%
+      mutate(.goffset=0-center, center=NULL) %>%
+      add_class("tbl_contig") %>%
+      drop_layout(keep = c("strand", ".goffset")) %>%
+      layout_contigs
 
   layout(x)
 }
