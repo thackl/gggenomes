6d7a9e642782ad9f39a19d104ffe72bfcd93cd80
Thomas Hackl <thackl@lim4.de>
2020-04-05 17:23:05 +0200

    wrap seqs (experimental)

--- R/seqs.R
+++ R/seqs.R
@@ -39,7 +39,7 @@ as_seqs.tbl_df <- function(x, everything=TRUE, ...){
     x$strand <- 1L
   }
   if(!has_name(x, "bin_offset")) x$bin_offset <- 0
-  if(!has_name(x, "start")) x$start <- 0
+  if(!has_name(x, "start")) x$start <- 1
   if(!has_name(x, "end")) x$end <- x$length
 
   other_vars <- if(everything) tidyselect::everything else function() NULL;
@@ -55,7 +55,7 @@ as_seqs.tbl_df <- function(x, everything=TRUE, ...){
 #' @param spacing between sequences in bases (>1) or relative to longest bin (<1)
 #' @param ... not used
 #' @export
-layout_seqs <- function(x, spacing=0.05,
+layout_seqs <- function(x, spacing=0.05, wrap=NULL,
     spacing_style = c("regular", "center", "spread"), keep="strand"){
 
   spacing_style <- match.arg(spacing_style)
@@ -73,9 +73,15 @@ layout_seqs <- function(x, spacing=0.05,
     spacing <- ceiling(max(len$length) * spacing)
   }
 
-  # compute contig offsets and compose layout
+  # compute seq starts in layout
+  if(is.null(wrap)){
+    x %<>% mutate(x = bin_offset + lag(cumsum(end-start+1 + spacing), default=0))
+  }else{
+    x %<>% wrap(wrap, spacing)
+  }
+
+  # fix strands
   x %<>% mutate(
-    x = bin_offset + lag(cumsum(end-start+1 + spacing), default=0),
     xend = dplyr::if_else(strand == -1, x, x+end-start+1),
     x = dplyr::if_else(strand == -1, x+end-start+1, x)
   ) %>%
@@ -87,4 +93,43 @@ drop_seq_layout <- function(x, keep="strand"){
   drop <- c("y","x","xend","strand", grep("^\\.", names(x), value=T))
   drop <- drop[!drop %in% keep]
   discard(x, names(x) %in% drop)
+}
+
+# layout contigs in rectangle
+wrap <- function(.data, xmax, xpad=1000){
+  l <- .data %>% thacklr::split_by(bin_id)
+  for(i in seq_along(l)){
+    ystart <- if(i == 1) 1 else max(l[[i-1]]$y) +2
+    xstart <- l[[i]]$bin_offset[1]
+    l[[i]] <- wrap_impl(l[[i]], xmax=xmax, xpad=xpad, ystart=ystart, xstart=xstart)
+  }
+  bind_rows(l)
+}
+
+
+
+wrap_impl <- function(.data, xmax, xpad, ystart, xstart){
+  l <- .data$end - .data$start # subseq length
+  n <- length(l)
+  x <- 1
+  xend <- x + l[1]
+  y <- ystart
+
+  if(n>1){
+    for(i in 2:n){
+      if(xend[i-1] + xpad + l[i] > xmax){
+        x[i] <- 1
+        xend[i] <- 1 + l[i]
+        y[i] <- y[i-1] + 1
+      }else{
+        x[i] <- xend[i-1] + xpad
+        xend[i] <- x[i] + l[i]
+        y[i] <- y[i-1]
+      }
+    }
+  }
+  .data$x <- x + xstart
+  .data$xend <- xend + xstart
+  .data$y <- y
+  .data
 }
