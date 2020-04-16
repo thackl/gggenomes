3a87d96f0146479d5a173daad12201c8f045f941
Thomas Hackl <thackl@lim4.de>
2020-04-16 13:49:11 +0200

    new functions to handle strand_*

---
+++ R/strand.R
@@ -0,0 +1,130 @@
+# check strand  ------------------------------------------------------------
+#' @export
+check_strand <- function(strand, check, na){
+  UseMethod("check_strand")
+}
+
+#' @export
+check_strand.factor <- function(strand, na=NA){
+  check_strand(as.character(strand), na=na)
+}
+
+#' @export
+check_strand.numeric <- function(strand, na=NA){
+  strand <- as.integer(strand)
+  if(any(!strand %in% c(1,-1,0,NA)))
+    rlang::abort("the only allowed values for numeric strands are [1,-1,0,NA]")
+  to_na <- !strand %in% c(1,-1, na)
+  strand[to_na] <- as.integer(na)
+  strand
+}
+
+#' @export
+check_strand.character <- function(strand, na=NA){
+  if(any(!strand %in% c("+", "-", ".", NA)))
+    rlang::abort("the only allowed values for character strands are [+,-,.,NA]")
+  to_na <- !strand %in% c("+", "-", na)
+  strand[to_na] <- as.character(na)
+  strand
+}
+
+#' @export
+check_strand.logical <- function(strand, na=NA){
+  strand[is.na(strand)] <- as.logical(na)
+  strand
+}
+
+
+# chr ------------------------------------------------------------
+#' @export
+strand_chr <- function(strand, na=NA){
+  UseMethod("strand_chr")
+}
+
+#' @export
+strand_chr.character <- function(strand, na=NA){
+  check_strand(strand, na=na)
+}
+
+#' @export
+strand_chr.factor <- function(strand, na=NA){
+  strand_chr(as.character(strand), na=na)
+}
+
+#' @export
+strand_chr.numeric <- function(strand, na=NA){
+  strand <- check_strand(strand, na=0)
+  strand <- c("-",as.character(na),"+")[strand+2]
+  strand
+}
+
+#' @export
+strand_chr.logical <- function(strand, na=NA){
+  # convert to int first
+  strand_chr(strand_int(strand, na=NA), na=na)
+}
+
+
+# int ------------------------------------------------------------
+#' @export
+strand_int <- function(strand, na=NA){
+  UseMethod("strand_int")
+}
+
+#' @export
+strand_int.numeric <- function(strand, na=NA){
+  check_strand(strand, na=na)
+}
+
+#' @export
+strand_int.logical <- function(strand, na=NA){
+  strand <- check_strand(strand, na=NA)
+  strand <- as.integer(strand) * 2 - 1
+  strand[is.na(strand)] <- as.integer(na)
+  as.integer(strand)
+}
+
+#' @export
+strand_int.character <- function(strand, na=NA){
+  strand <- check_strand(strand, na=NA)
+  strand[strand == "+"] <- 1
+  strand[strand == "-"] <- -1
+  strand[is.na(strand)] <- as.integer(na)
+  as.integer(strand)
+}
+
+#' @export
+strand_int.factor <- function(strand, na=NA){
+  strand_int(as.character(strand), na=na)
+}
+
+# lgl ------------------------------------------------------------
+#' @export
+strand_lgl <- function(strand, na=NA){
+  UseMethod("strand_lgl")
+}
+
+#' @export
+strand_lgl.logical <- function(strand, na=NA){
+  check_strand(strand, na=na)
+}
+
+#' @export
+strand_lgl.numeric <- function(strand, na=NA){
+  strand <- as.logical(check_strand(strand, na=NA) +1)
+  strand[is.na(strand)] <- as.logical(na)
+  strand
+}
+
+#' @export
+strand_lgl.character <- function(strand, na=NA){
+  strand <- check_strand(strand, na=NA)
+  strand <- strand == "+"
+  strand[is.na(strand)] <- as.logical(na)
+  strand
+}
+
+#' @export
+strand_rev <- function(strand, na=FALSE){
+  !strand_lgl(strand, na=na)
+}
