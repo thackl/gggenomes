f79d9cc6a522cf46ee248445a02ac2c180f34606
Thomas Hackl <thackl@lim4.de>
2020-05-10 22:02:18 +0200

    minor changes to gggenomes/gggenomes_layout doc and defaults; .group_vars for use_bins()

--- R/gggenomes.R
+++ R/gggenomes.R
@@ -5,16 +5,42 @@
 #' Feature data: `read_gff`
 #'
 #' Link data: `read_paf`
-#' @inheritParams layout_genomes
-#' @inheritParams ggplot2::ggplot
-#' @param ... layout parameters passed on to `layout_genomes()`
+#' @param seqs a table with sequence data (seq_id, bin_id, length)
+#' @param genes a table or a list of table with gene data to be added as feature
+#' tracks. Required columns: seq_id, bin_id, start, end.
+#'
+#' For a single table, adds the track_id will be "genes". For a list, track_ids
+#' are parsed from the list names, or if names are missing from the name of the
+#' variable containing each table.
+#' @param features same as genes, but the single table track_id will default to
+#' "features".
+#' @param links a table or a list of tables with link data to be added as link
+#' tracks (columns: from, to, from_start, from_end, to_start, to_end). Same
+#' naming scheme as for features.
+#' @param infer_length,infer_start,infer_end,infer_bin_id used to infer pseudo
+#' seqs if only features or links are provided, or if no bin_id column was
+#' provided. The expressions are evaluated in the context of the first feature
+#' or link track.
+#'
+#' By default subregions of sequences from the first to the last feature/link
+#' are generated. Set `infer_start` to 0 to show all sequences from their
+#' true beginning.
+#' @param ... layout parameters passed on to [layout_seqs()]
 #' @param theme choose a gggenomes default theme, NULL to omit.
-#' @param .layout a pre-computed layout from `layout_genomes()` (advanced)
+#' @param .layout a pre-computed layout from [layout_genomes()]. Useful for
+#' developmental purposes.
 #' @import ggplot2 grid rlang
 #' @export
 #' @return gggenomes-flavored ggplot object
+#' @examples
+#' seq_df <- tribble(
+#'   bin_id = c("A", "A", "B"),
+#'   seq_id = c("a1", "a2", "b1"),
+#'   length = c(1000, 2000, 2500))
+#' gggenomes()
 gggenomes <- function(seqs=NULL, genes=NULL, features=NULL, links=NULL, ...,
-    theme = c("clean", NULL), .layout=NULL){
+    infer_bin_id = seq_id, infer_start = min(start,end), infer_end = max(start,end),
+    infer_length = max(start,end), theme = c("clean", NULL), .layout=NULL){
 
   # parse track_args to tracks - some magic for a convenient api
   genes_exprs <- enexpr(genes)
@@ -35,6 +61,8 @@ gggenomes <- function(seqs=NULL, genes=NULL, features=NULL, links=NULL, ...,
 
   p <- p + scale_y_continuous("", expand = expand_scale(add=.5),
       trans = scales::reverse_trans())
+
+  p <- p + xlab("Nucleotide position")
 
   theme_name <- theme[[1]] %||% match.arg(theme[[1]], c("clean"))
   if(!is.null(theme_name)){ # add theme
@@ -70,33 +98,9 @@ ggplot.gggenomes_layout <- function(data, mapping = aes(), ...,
   ggplot2:::set_last_plot(p)
   p
 }
-
-#' Compute a genomes layout from sequences, features and links
-#'
-#' See `gggenomes::gggenomes()` for more info.
-#'
-#' @param seqs a table with sequence data (seq_id, bin_id, length)
-#' @param genes a table or a list of table with gene data to be added as feature
-#' tracks. Required columns: seq_id, bin_id, start, end.
-#'
-#' For a single table, adds the track_id will be "genes". For a list, track_ids
-#' are parsed from the list names, or if names are missing from the name of the
-#' variable containing each table.
-#' @param features same as genes, but the single table track_id will default to
-#' "features".
-#' @param links a table or a list of tables with link data to be added as link
-#' tracks (columns: from, to, from_start, from_end, to_start, to_end). Same
-#' naming scheme as for features.
-#' @param infer_bin_id,infer_start,infer_end,infer_length used to infer pseudo
-#' seqs if only features or links are provided. The expressions are evaluated in
-#' the context of the first feature track, or the first links track.
-#'
-#' By default subregions of sequences from the first to the last feature/link
-#' are generated. Set `infer_start` to 0 to show all sequences from their
-#' true beginning.
-#' @param features_track_id default ID for first features track
-#' @param links_track_id default ID for first links track
-#' @param ... layout parameters passed on to `layout_seqs()`
+#' @rdname gggenomes
+#' @inheritParams gggenomes
+#' @return gggenomes_layout object
 #' @export
 layout_genomes <- function(seqs=NULL, genes=NULL, features=NULL, links=NULL,
     infer_bin_id = seq_id, infer_start = min(start,end), infer_end = max(start,end),
@@ -189,7 +193,7 @@ infer_seqs_from_links <- function(links, infer_bin_id = seq_id, infer_start = mi
 #' @importFrom ggplot2 theme
 #' @inheritParams ggplot2::theme_bw
 #' @export
-theme_gggenomes_clean <- function(base_size = 24, base_family = "", base_line_size = base_size/22, base_rect_size = base_size/22){
+theme_gggenomes_clean <- function(base_size = 16, base_family = "", base_line_size = base_size/22, base_rect_size = base_size/22){
   theme_bw(
     base_size = base_size, base_family = base_family,
     base_line_size = base_line_size, base_rect_size = base_rect_size
