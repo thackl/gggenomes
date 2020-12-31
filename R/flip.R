bf7879af789f635948d8472a63bb3dc98b266c73
Thomas Hackl <thackl@lim4.de>
2020-12-31 05:23:17 +0100

    flip(where())

--- R/flip.R
+++ R/flip.R
@@ -1,16 +1,19 @@
 #' Flip bins and sequences
 #'
-#' \code{flip} and \code{flip_seq} reverse-complement specified bins or
+#' \code{flip} and \code{flip_seqs} reverse-complement specified bins or
 #' individual sequences and their features. \code{flip_nicely} automatically
 #' flips bins using a heuristic that maximizes the amount of forward strand
 #' links between neighboring bins.
 #' @param x a gggenomes object
 #' @param ... bins or sequences to flip in dplyr::select like syntax (numeric
-#'   position or unquoted expressions)
+#'     position or unquoted expressions)
 #' @param .bins preselection of bins with sequences to flip. Useful if selecting
-#'   by numeric position. It sets the context for selection, for example the
-#'   11th sequences of the total set might more easily described as the 2nd
-#'   sequences of the 3rd bin: \code{flip_seq(2, bins=3)}
+#'     by numeric position. It sets the context for selection, for example the
+#'     11th sequences of the total set might more easily described as the 2nd
+#'     sequences of the 3rd bin: \code{flip_seqs(2, .bins=3)}
+#' @param .bin_track,.seq_track when using a function as selector such as
+#'     \link{\code{tidyselect::where}}, this specifies the track in which
+#'     context the function is evaluated.
 #' @examples
 #' p <- gggenomes(emale_seqs[1:7,], links=emale_links) +
 #'   geom_seq(aes(color=strand), arrow=TRUE) + geom_link() +
@@ -32,33 +35,34 @@
 #'   add_clusters(genes, emale_cogs) %>%
 #'   flip_nicely(emale_cogs)
 #' @export
-flip <- function(x, ...){
+flip <- function(x, ..., .bin_track=seqs){
   UseMethod("flip")
 }
 #' @export
-flip.gggenomes <- function(x, ...){
-  x$data <- flip(x$data, ...)
+flip.gggenomes <- function(x, ..., .bin_track=seqs){
+  x$data <- flip(x$data, ..., .bin_track={{ .bin_track }})
   x
 }
 #' @export
-flip.gggenomes_layout <- function(x, ...){
+flip.gggenomes_layout <- function(x, ..., .bin_track=seqs){
   if(!has_dots()) return(x)
-  flip_impl(x, .bins=c(...))
+  flip_impl(x, bins=c(...), bin_track={{.bin_track}})
 }
 #' @rdname flip
 #' @export
-flip_seq <- function(x, ..., .bins=everything()){
-  UseMethod("flip_seq")
+flip_seqs <- function(x, ..., .bins=everything(), .seq_track=seqs, .bin_track=seqs){
+  UseMethod("flip_seqs")
 }
 #' @export
-flip_seq.gggenomes <- function(x, ..., .bins=everything()){
-  x$data <- flip_seq(x$data, ..., .bins = {{ .bins }})
+flip_seqs.gggenomes <- function(x, ..., .bins=everything(), .seq_track=seqs, .bin_track=seqs){
+  x$data <- flip_seqs(x$data, ..., .bins={{.bins}}, .bin_track={{.bin_track}},
+      .seq_track={{.seq_track}})
   x
 }
 #' @export
-flip_seq.gggenomes_layout <- function(x, ..., .bins=everything()){
+flip_seqs.gggenomes_layout <- function(x, ..., .bins=everything(), .seq_track=seqs, .bin_track=seqs){
   if(!has_dots()) return(x)
-  flip_impl(x, ..., .bins = {{ .bins }})
+  flip_impl(x, bins={{.bins}}, seqs=c(...), bin_track={{.bin_track}}, seq_track={{.seq_track}})
 }
 #' @rdname flip
 #' @param link_track the link track to use for flipping bins nicely
@@ -105,32 +109,37 @@ flip_nicely.gggenomes_layout <- function(x, link_track=1, min_coverage=.2){
   x %>% flip(all_of(y_flip))
 }
 
-flip_impl <- function(x, ..., .bins=everything()){
+flip_impl <- function(x, bins=everything(), seqs=NULL, bin_track=seqs, seq_track=seqs){
   # split by bin_id and select bins
-  s <- seqs(x)
-  l <- s %>% thacklr::split_by(bin_id)
-  i <- tidyselect::eval_select(expr({{ .bins }}), l)
-  if(length(i) == 0) rlang::abort("no bins selected")
-  s <- bind_rows(l[i])
-
-
+  seq_tbl <- pull_seqs(x)
+  seq_lst <- split_by(seq_tbl, bin_id)
+  # in case we want to compute selections based on a track other than seqs
+  bin_sel_lst <- split_by(pull_track(x, {{bin_track}}), bin_id)
+  bin_i <- tidyselect::eval_select(expr({{ bins }}), bin_sel_lst)
+  if(length(bin_i) == 0) rlang::abort("no bins selected")
+  # select bins to operate on
+  flip_tbl <- bind_rows(seq_lst[names(bin_i)])
+    
   # flip seqs in bins
-  if(length(ellipsis:::dots()) > 0){
-    seq_ids <- s$seq_id %>% set_names(.)
-    j <- tidyselect::eval_select(expr(c(...)), seq_ids)
-    s$strand[j] <- flip_strand(s$strand[j])
+  seqs <- enquo(seqs)
+  if(!quo_is_null(seqs)){
+    seq_sel_lst <- split_by(pull_track(x, {{seq_track}}), seq_id)
+    seq_i <- tidyselect::eval_select(expr(!! seqs ), seq_sel_lst)
+    seq_i <- flip_tbl$seq_id %in% names(seq_i)
+    flip_tbl$strand[seq_i] <- flip_strand(flip_tbl$strand[seq_i])
   # flip entire bins
   }else{
-    s %<>% mutate(strand = flip_strand(strand)) %>%
-      arrange(-row_number())
+    flip_tbl %<>% group_by(bin_id) %>%
+      mutate(strand = flip_strand(strand)) %>%
+      arrange(-row_number(), .by_group=TRUE)
   }
 
   # splice modified bins back into rest
-  m <- s %>% thacklr::split_by(bin_id)
-  l[names(m)] <- m
-  s <- bind_rows(l)
+  flip_lst <- flip_tbl %>% thacklr::split_by(bin_id)
+  seq_lst[names(flip_lst)] <- flip_lst
+  seq_tbl <- bind_rows(seq_lst)
 
-  seqs(x) <- s
+  seqs(x) <- seq_tbl
   layout(x)
 }
 
