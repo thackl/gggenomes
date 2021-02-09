9dfcb28d12afd130480469840195e58951f25535
Thomas Hackl <thackl@lim4.de>
2021-02-09 23:01:17 +0100

    parse desc w/o key=val and parse ncbi-style [key=value]

--- R/read_seqs.R
+++ R/read_seqs.R
@@ -27,11 +27,11 @@ read_seqs <- function(files, .id="file_id", format=NULL, parser=NULL,
   seqs
 }
 
-parse_desc <- function(x, pattern="\\s*(\\S+)=\\{?([^=]+?)(\\s|\\}|$)"){
+parse_desc <- function(x, pattern="\\s*\\[?(\\S+)=\\{?([^=]+?)(\\s|\\}|\\]|$)"){
   m <- str_match_all(x, pattern) # create  list of match matrices
   y <- map_df(m, function(.x){
     # if x was NA, all match values are NA - return empty tibble with one row
-    if(any(is.na(.x[,2]))) return(tibble(.rows=1))
+    if(nrow(.x) < 1 || any(is.na(.x[,2]))) return(tibble(.rows=1))
     # else return tibble with keys as names
     .x[,3] %>% as.list %>% set_names(.x[,2]) %>% as_tibble
   }) %>% mutate(across(everything(), type.convert, as.is=TRUE))
