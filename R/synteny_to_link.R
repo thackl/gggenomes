3f86e1e803da330882445f2dad717eab5afa8766
Niklas Schandry <n.schandry@bio.lmu.de>
2021-10-20 16:37:47 +0200

    Update synteny_to_link.R
    Cleaned up

--- R/synteny_to_link.R
+++ R/synteny_to_link.R
@@ -1,43 +1,41 @@
 #' Helper to extract seqlengths from DECIPHER::FindSyntney Object
 #' @details Extracts the sequence lengths 
 #' @param synteny_obj A object produced by DECIPHER::FindSynteny
-#' @param seqnames A vector containing the sequence names that should be output, should correspond to the number of sequences in the sequence DB
 
-get_seqlen <- function(synteny_obj, seqnames) {
+get_seqlen <- function(synteny_obj) {
   seqlengths <- c()
-  for(i in 1:length(seqnames)) {
+  for(i in 1:ncol(synteny_obj)) {
     seqlengths <- c(seqlengths,unlist(synteny[i,i]))
   }
   seqlength_df <- data.frame(length = seqlengths,
-             fasta_name = names(seqlengths),
-             name = seqnames)
-  seqlength_df %>% remove_rownames()
+             fasta_name = names(seqlengths)) %>%
+    tibble::remove_rownames()
+  seqlength_df 
 }
 
 #' Format synteny-class into wide tibble This is for use with gggenomes
 #' @details This function takes a object of class synteny and converts it into a data.frame containing all synteny pairs
 #' @param synteny_obj A object produced by DECIPHER::FindSynteny
-#' @param seqnames A vector containing the sequence names that should be output, should correspond to the number of sequences in the sequence DB
 
-synteny_to_link <- function(synteny_obj, seqnames) {
+synteny_to_link <- function(synteny_obj) {
   if(class(synteny_obj) != "Synteny") {
     stop("synetny_obj is not of class Synteny")
   }
-  seqlengths <- get_seqlen(synteny_obj, seqnames)
+  seqlengths <- get_seqlen(synteny_obj)
     out_df <- tibble()
-  for (iter in c(1:(length(seqnames)-1))) {
+  for (iter in c(1:(ncol(synteny_obj)-1))) {
     tmp_df <- synteny_obj[iter+1,iter][[1]] %>% 
       as.data.frame() %>% 
       dplyr::mutate(strand = case_when(strand == 0 ~ "+",
                                        TRUE ~ "-"),
-                    seq_id = (seqlengths[seqlengths$name == seqnames[iter],]$fasta_name %>% str_extract("[^ ]+")),
+                    seq_id = (seqlengths[iter,]$fasta_name %>% str_extract("[^ ]+")),
                     start = start1,
                     end = end1,
-                    length = seqlengths[seqlengths$name == seqnames[iter],]$length,
-                    seq_id2 = (seqlengths[seqlengths$name == seqnames[iter+1],]$fasta_name %>% str_extract("[^ ]+")),
+                    length = seqlengths[iter,]$length,
+                    seq_id2 = (seqlengths[iter+1,]$fasta_name %>% str_extract("[^ ]+")),
                     start2 = start2,
                     end2 = end2,
-                    length2 = seqlengths[seqlengths$name == seqnames[iter+1],]$length) %>% 
+                    length2 = seqlengths[iter+1,]$length) %>% 
       dplyr::select(seq_id, start, end,length, strand, seq_id2, start2, end2, length2)
   out_df <- bind_rows(out_df, tmp_df)
   }
