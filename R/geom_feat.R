bf3234a99a187d3bf6c273ca802417cddd2b7e6f
BvAdrichem <117816868+BvAdrichem@users.noreply.github.com>
2023-01-02 15:32:53 +0100

    Update geom_feat.R

--- R/geom_feat.R
+++ R/geom_feat.R
@@ -23,7 +23,7 @@ geom_feat <- function(mapping = NULL, data = feats(), stat="identity",
 ##   mapping <- aes_nudge_by_strand(mapping, nudge_by_strand)
 
 ##   # TODO: would be cleaner with GeomFeat ggproto...
-##   if (has_name(mapping, "size")) aes_intersect(mapping, aes(size = 3))
+##   if (has_name(mapping, "linewidth")) aes_intersect(mapping, aes(linewidth = 3))
 ##   if (any(has_name(mapping, c("color", "colour")))) aes_intersect(mapping, aes(color = "grey40"))
 ##   r <- list(geom_segment(mapping = mapping, data = data, ...))
 
@@ -31,12 +31,12 @@ geom_feat <- function(mapping = NULL, data = feats(), stat="identity",
 ##     if(!inherits(arrow, "arrow")) arrow <- grid::arrow(length = unit(2, "mm"))
 ##     r <- c(r, list(
 ##       geom_segment(aes(x=ifelse(x<xend, xend-1, xend+1), y, xend=xend, yend=y), data=data,
-##                    arrow=arrow, size=0.5, color="grey85")))
+##                    arrow=arrow, linewidth=0.5, color="grey85")))
 ##   }
 ##   r
 ## }
 
 GeomFeat <- ggproto(
   "GeomFeat", GeomSegment,
-  default_aes = aes(colour = "paleturquoise4", size = 2, linetype = 1, alpha = NA)
+  default_aes = aes(colour = "paleturquoise4", linewidth = 2, linetype = 1, alpha = NA)
 )
