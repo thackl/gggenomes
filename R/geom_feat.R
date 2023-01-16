9f1560ba5dca3c3f2ed0c5933035798eca1f1085
BvAdrichem <117816868+BvAdrichem@users.noreply.github.com>
2023-01-16 13:31:09 +0100

    Update geom_feat
    changed size to linewidth for geom_feat.
    ggplot2::geom_segment uses linewidth since latest update (version 3.4.0)

--- R/geom_feat.R
+++ R/geom_feat.R
@@ -38,5 +38,5 @@ geom_feat <- function(mapping = NULL, data = feats(), stat="identity",
 
 GeomFeat <- ggproto(
   "GeomFeat", GeomSegment,
-  default_aes = aes(colour = "paleturquoise4", size = 2, linetype = 1, alpha = NA)
+  default_aes = aes(colour = "paleturquoise4", linewidth = 2, linetype = 1, alpha = NA)
 )
