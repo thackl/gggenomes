ffc7682b10929705290c7b25aff6d83e9c21c89c
Thomas Hackl <thackl@lim4.de>
2025-10-10 22:32:11 +0200

    explicit call to utils::readCitationFile

--- R/zzz.R
+++ R/zzz.R
@@ -1,6 +1,6 @@
 .onAttach <- function(libname, pkg) {
   pkg_ver <- paste0(pkg, " v", utils::packageDescription(pkg, fields="Version"))
   citfile <- file.path(system.file(package = pkg), "CITATION")
-  cit <- format(readCitationFile(citfile), style="text")
+  cit <- format(utils::readCitationFile(citfile), style="text")
   packageStartupMessage(paste0(pkg_ver, "\n\nIf you use '", pkg, "' in published research, please cite:\n\n", cit))
 }
