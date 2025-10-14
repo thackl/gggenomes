cbd48ff1eba22d2d23c8460ffffbc1d54dbb4b57
Thomas Hackl <thackl@lim4.de>
2025-10-14 04:30:18 +0200

    Fixed align example

--- R/align.R
+++ R/align.R
@@ -2,19 +2,20 @@
 #' 
 #' Align your genomes relative to target features, such as genes or regions of
 #' interest. Use the `...` argument to indicate a subset of features in one
-#' track as anchor points. If multiple features are selected per bin, they are
+#' track as targets. If multiple features are selected per bin, they are
 #' treated as a single feature spanning from the leftmost to the rightmost end.
-#' The genomes will be shifted so that the anchors align according to the
-#'  `.align`.
-#' @param x gggenomes object    
+#' The genomes will be shifted so that the targets features align according to
+#' the `.justify`.
+#' @param x gggenomes object
 #' @param ... filter expression to identify target features in target track.
 #' Works like `dplyr::filter()` <[`data-masking`][rlang::args_data_masking]>.
 #' @param .track_id track to pull from, default "genes"
-#' @param .align alignment position, one of "left", "center", "right" or numeric
-#'   between 0 and 1, default "left"
+#' @param .justify alignment position, one of "left", "center", "right" or
+#' numeric between 0 and 1, default "left"
 #' @return gggenomes object with shifted coordinates.
 #' @export
 #' @examples
+#' library(patchwork)
 #' p <- gggenomes(emale_genes, links = emale_ava) +
 #'   geom_link() +
 #'   geom_gene(aes(fill = name)) +
@@ -27,16 +28,16 @@
 #'   # left-align on MCP gene after flipping bins
 #'   p |> sync() |> align(name == "MCP") +
 #'   # right-align on MCP gene
-#'   p |> align(name == "MCP", .align = "right") +
+#'   p |> align(name == "MCP", .justify = "right") +
 #'   # center-align on MCP + pri-hel gene
-#'   p |> sync() |> align(name %in% c("MCP", "pri-hel"), .align = "center") |>
+#'   p |> sync() |> align(name %in% c("MCP", "pri-hel"), .justify = "center") |>
 #'     # and highlight the feature block we are aligning to
 #'     locate(name %in% c("MCP", "pri-hel"), .expand = 0, .max_dist = 1e6) +
-#'     geom_feat(data = feats(loci), color = "plum3", alpha = .5, linewidth = 5) +
+#'     geom_feat(data = feats(loci), color = "plum3", alpha = .5, linewidth = 5)
 #'   # center-align by fraction on MCP and pri-hel gene (hjust-like behaviour)
-#'   p |> align(name %in% c("MCP", "pri-hel"), .align = .5) +
+#'   p |> align(name %in% c("MCP", "pri-hel"), .justify = .5) +
 #'   # right-align by fraction after flipping
-#'   p |> sync() |> align(name %in% c("MCP", "pri-hel"), .align = 1)
+#'   p |> sync() |> align(name %in% c("MCP", "pri-hel"), .justify = 1)
 #' 
 #' pp + plot_layout(guides = "collect") & geom_vline(xintercept = 0, linetype = 2)
 #' 
@@ -55,21 +56,21 @@
 #' 
 #' pp <-
 #'   # center on everything - just omit ...
-#'   p |> align(.track_id = "seqs", .align = .5) +
+#'   p |> align(.track_id = "seqs", .justify = .5) +
 #'   # right-align on contig ending in "2"
 #'   # NOTE: there is no 2nd contig in bin A, so nothing is aligned there
-#'   p |> align(str_detect(seq_id, "2"), .track_id = "seqs", .align = "right")
+#'   p |> align(str_detect(seq_id, "2"), .track_id = "seqs", .justify = "right")
 #' 
 #' pp + plot_layout(guides = "collect") & geom_vline(xintercept = 0, linetype = 2)
-align <- function(x, ..., .track_id = "genes", .align = "left"){
-  if(is.character(.align)){
-    .align <- c(left=0, center=.5, right=1)[.align]
+align <- function(x, ..., .track_id = "genes", .justify = "left"){
+  if(is.character(.justify)){
+    .justify <- c(left=0, center=.5, right=1)[.justify]
   }
-  if(is.na(.align)){stop("align required")}
+  if(is.na(.justify)){stop("align required")}
 
   a <- x |> 
     pull_track(.track_id = .track_id, ...) |> 
     dplyr::group_by(bin_id) |> 
-    dplyr::summarize(x = ((1-.align) * min(c(x, xend))) + (.align * max(c(x, xend))))
+    dplyr::summarize(x = ((1-.justify) * min(c(x, xend))) + (.justify * max(c(x, xend))))
 	shift(x, bins = a$bin_id, by=-a$x)
 }
